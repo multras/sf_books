37d9158c39b113e5b10ed13cc6585550d8e7f0d3
Sebastian Fischer <typo3@evoweb.de>
2019-01-30 18:34:38 +0100

    Raise version

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ books and to search easily information about each book.',
     'author_company' => 'evoWeb',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
-    'version' => '4.1.0',
+    'version' => '5.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-9.5.99',
