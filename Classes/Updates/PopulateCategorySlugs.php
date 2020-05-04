52ccedc9ad2ac8689b1aef5c12f24883ad34c097
Sebastian Fischer <typo3@evoweb.de>
2020-05-04 12:25:33 +0200

    Add path_segment updates

---
+++ Classes/Updates/PopulateCategorySlugs.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is copied from the TYPO3 CMS install tool package.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace Evoweb\SfBooks\Updates;
+
+/**
+ * Fills tx_sfbooks_domain_model_category.path_segment with a proper value for pages that do not have a slug updater.
+ * Does not take "deleted" authors into account, but respects workspace records.
+ *
+ * @internal This class is only meant to be used within EXT:install and is not part of the TYPO3 Core API.
+ */
+class PopulateCategorySlugs extends AbstractPopulateSlugs
+{
+    /**
+     * @var string
+     */
+    protected $table = 'tx_sfbooks_domain_model_category';
+
+    /**
+     * @var string
+     */
+    protected $fieldName = 'path_segment';
+
+    /**
+     * @return string Unique identifier of this updater
+     */
+    public function getIdentifier(): string
+    {
+        return 'sfBooksCategoriesSlugs';
+    }
+
+    /**
+     * @return string Title of this updater
+     */
+    public function getTitle(): string
+    {
+        return 'Introduce URL parts ("slugs") to all existing categories';
+    }
+
+    /**
+     * @return string Longer description of this updater
+     */
+    public function getDescription(): string
+    {
+        return 'TYPO3 includes native URL handling. Every category record has its own speaking URL path'
+            . ' called "path_segment" which can be edited in TYPO3 Backend. However, it is necessary'
+            . ' that all categories have a URL pre-filled. This is done by evaluating the category title.';
+    }
+}
